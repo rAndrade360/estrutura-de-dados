9ea36b469ef262367ed89d7caea15f7180998bd0
Renan <renan@pop-os.localdomain>
2020-09-09 19:46:16 -0300

    Creating main function and refactoring

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct  node {
+struct node {
   int value;
   struct node* next;   
 };
@@ -15,11 +16,18 @@ struct list {
 
 typedef struct list List;
 
-List* initializeList(void);
-Node* createNode(int);
-
-int main() {
-  printf("\nHello!\n");
+void showMenu(void) {
+    printf("\n--------Menu---------");
+    printf("\n1. Insert at the beginning of list");
+    printf("\n2. Insert at the middle of list");
+    printf("\n3. Insert at the end of list");
+    printf("\n4. Remove from the beginning of list");
+    printf("\n5. Remove from the middle of list");
+    printf("\n6. Remove from the end of list");
+    printf("\n7. Print list");
+    printf("\n8. Search node by position");
+    printf("\n9. Search node by value");
+    printf("\n0. exit");
 }
 
 List* initializeList(void) {
@@ -116,6 +124,9 @@ void removeFromTheMiddle(List* list, int position) {
 
 void printList(List* list) {
     Node* current = list->start;
+    if(list->size == 0) {
+      printf("\nEmpty list!\n");
+    }
     for (int i = 1; i <= list->size; i++) {
         printf("%d --> ", current->value);
         current = current->next;
@@ -128,15 +139,17 @@ Node* searchNodeByPosition(List* list, int position) {
     return result;
 }
 
-Node* searchNodeByValue(List* list, int value) {
+int verifyIfExistsValue(List* list, int value) {
     Node* current = list->start;
+    int exists = 0;
       for (int i = 1; i < list->size; i++) {
           if(current->value == value) {
+              exists = 1;
               break;
           }
         current = current->next;
       }
-    return current;
+    return exists;
 }
 
 Node* getCurrentNode(List* list, int limitValue) {
@@ -145,4 +158,88 @@ Node* getCurrentNode(List* list, int limitValue) {
         current = current->next;
       }
     return current;
+}
+
+int main() {
+  int option;
+  int value, position;
+  List* list = initializeList();
+
+  do
+  {
+    showMenu();
+    
+    printf("\n Choose an option: ");
+    scanf("%d", &option);
+    switch (option)
+    {
+    case 1:
+      printf("Enter a value to insert at the beginning of the list: ");
+      scanf("%d", &value);
+      insertAtTheBeginning(list, value);
+        break;
+
+    case 2:
+      printf("\nEnter a value to insert at the middle of the list: ");
+      scanf("%d", &value);
+      printf("\nEnter an position to insert the value: ");
+      scanf("%d", &position);
+      insertInTheMiddle(list, value, position);
+        break;
+
+    case 3:
+      printf("\nEnter a value to insert at the end of the list: ");
+      scanf("%d", &value);
+      insertAtTheEnd(list, value);
+        break;
+
+    case 4:
+      printf("\nRemoving from the beginning...\n");
+      removeFromTheBeginning(list);
+        break;
+
+    case 5:
+      printf("\nEnter a position to remove from the middle of the list: ");
+      scanf("%d", &position);
+      removeFromTheMiddle(list, position);
+        break;
+
+    case 6:
+      printf("\nRemoving from the end...\n");
+      removeFromTheEnd(list);
+        break;
+
+    case 7:
+      printf("\nList:\n");
+      printList(list);
+      printf("\n");
+        break;
+
+    case 8:
+      printf("\nEnter a position to search node: ");
+      scanf("%d", &position);
+      Node* node = searchNodeByPosition(list, position);
+      printf("\nNode value: %d", node->value);
+        break;
+
+    case 9:
+      printf("\nEnter a value to search node: ");
+      scanf("%d", &value);
+      int exists = verifyIfExistsValue(list, value);
+      if(exists == 1) {
+          printf("\nYeah! The value exists!\n");
+      }else{
+          printf("\nOh no! The value not exists!\n");
+      }
+        break;
+
+    case 0:
+      printf("\nExiting graciously...\n");
+    
+    default:
+      printf("\nInvalid option! :(\n");
+        break;
+    }
+  } while (option != 0);
+  
 }
