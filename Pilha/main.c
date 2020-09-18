2929262143fd4563159588b1b41f56c3c8b5fe31
Renan <r.andrade@acad.ifma.edu.br>
2020-09-18 14:34:52 -0300

    Creating structures in stack

--- Pilha/main.c
+++ Pilha/main.c
@@ -2,10 +2,38 @@
 #include <stdio.h>
 #include <string.h>
 
-int main () {
-  struct element
+
+struct element
   {
     struct element* next;
+    int value;
   };
+
+typedef struct element Element;
+
+struct stack
+{
+  struct element* start;
+  int size;
+};
+
+typedef struct stack Stack;
+
+Stack* initializeStack() {
+  Stack* stack = (Stack*) malloc(sizeof(Stack));
+  stack->start = NULL;
+  stack->size = 0;
+  return stack;
+}
+
+Element* createElement(int value) {
+  Element* element = (Element*) malloc(sizeof(Element));
+  element->value = value;
+  return element;
+}
+
+
+
+int main () {
   
 }
